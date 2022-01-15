1c8dfc776181a83d0b68448cbe06c8efd831f7e9
Lucky <luckych8080@gmail.com>
2022-01-16 03:11:34 +0530

    idk

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -4,6 +4,8 @@ import { useDispatch } from "react-redux";
 import { setAlert } from "../store/actions/alertActions";
 import { getWeather, setLoading } from "../store/actions/weatherActions";
 
+import {Container, Paper, Button, Stack, Typography} from "@mui/material";
+
 interface SearchProps {
   title: string;
 }
@@ -29,29 +31,28 @@ const Search: FC<SearchProps> = ({ title }) => {
   };
 
   return (
-    <div className="hero is-light has-text-centered">
-      <div className="hero-body">
-        <div className="container">
-          <h1 className="title">{title}</h1>
-          <form className="py-5" onSubmit={submitHandler}>
-            <input
-              type="text"
-              className="input has-text-centered mb-2"
-              placeholder="Enter city name"
-              style={{ maxWidth: 300 }}
-              value={city}
-              onChange={changeHandler}
-            />
-            <button
-              className="button is-primary is-fullwidth"
-              style={{ maxWidth: 300, margin: "0 auto" }}
-            >
-              Search
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
+    <Container maxWidth="sm" style={{marginTop: "20px"}}>
+      <Paper elevation={3} style={{}}>
+      <form onSubmit={submitHandler}>
+        <Stack spacing={2} style={{display: "flex", alignItems: "center", margin: "15px"}}>
+        <Typography variant="h4">{title}</Typography>
+        <input
+          type="text"
+          className="input has-text-centered mb-2"
+          placeholder="Enter city name"
+          style={{ maxWidth: 300 }}
+          value={city}
+          onChange={changeHandler}
+        />
+        <Button color="success" size="large"
+          style={{ maxWidth: 300, margin: "10px" }}
+        >
+          Search
+        </Button>
+        </Stack>
+      </form>
+      </Paper>
+    </Container>
   );
 };
 
